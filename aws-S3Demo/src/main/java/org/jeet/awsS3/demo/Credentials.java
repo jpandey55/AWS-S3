e7ce03e716ead521bbfcfd9be0ce9c20cc49981d
Jitendra Kumar Pandey <jpandey@buffalo.edu>
2016-08-17 19:52:57 -0400

    Update Credentials.java

--- aws-S3Demo/src/main/java/org/jeet/awsS3/demo/Credentials.java
+++ aws-S3Demo/src/main/java/org/jeet/awsS3/demo/Credentials.java
@@ -2,7 +2,7 @@ package org.jeet.awsS3.demo;
 
 public class Credentials {
 
-	public static final String access_key_id= "AKIAJUXLB5H7TKAVPTSA";
+	public static final String access_key_id= "AKIAJUXLO5H7TKAVPTSA";
 
     public static final String secret_access_key ="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";
 
